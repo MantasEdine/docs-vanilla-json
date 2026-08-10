fdb6c9ea709c8940fcf1ac37683156b654a75ccb
MantasEdine <youcefmantas3@gmail.com>
2026-08-10 13:19:12 +0100

    clipboard-copy-action-added

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 
 
-let clicboard = document.getElementById("copyme")
+let clickboard = document.getElementById("copyme");
+clickboard.addEventListener("click",()=>{
+        
+navigator.clipboard.writeText("npm i @rabia_youcef/vanilla-json").then(() => {
+    let oldText = clickboard.textContent   // save the STRING
+    clickboard.textContent = "Copied!"      // consistent name
+    setTimeout(() => { clickboard.textContent = oldText }, 1000)
+})
+})
 
 
